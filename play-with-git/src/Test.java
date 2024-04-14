ae6c41234a72aa903bc757f841ada4dee6053367
alexandruzarnoianu <alex.zarnoianu@gmail.com>
2024-04-14 19:52:45 +0300

    Ana chiar are mere.

--- play-with-git/src/Test.java
+++ play-with-git/src/Test.java
@@ -12,4 +12,17 @@ public class Test {
     public static int sum(int a, int b) {
         return a + b;
     }
+
+    public static boolean checkSum (int number) {
+        int a = 5;
+        int b = 2;
+        int sum = sum(a, b);
+        if (sum % 2 == 0) {
+            System.out.println("sum is even");
+            return true;
+        } else {
+            System.out.println("sum is odd");
+            return false;
+        }
+    }
 }
