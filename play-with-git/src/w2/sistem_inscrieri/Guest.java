1cdf41257fce37cd2c7c09d1aea3f38402b056ad
cvintilian.rosca <lalal@gmail.com>
2024-05-11 12:18:38 +0300

    Workshop 2: Added Guest Class

---
+++ play-with-git/src/w2/sistem_inscrieri/Guest.java
@@ -0,0 +1,40 @@
+package w2.sistem_inscrieri;
+
+public class Guest {
+    private String lastName;
+    private String firstName;
+    private String email;
+    private String phoneNumber;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
